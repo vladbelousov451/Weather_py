3a909b60d706a373b6e3d2147fd5275be3486713
vlad <vladibelousov54@gmail.com>
2020-08-16 16:25:52 +0300

    wrote the code

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from twilio.rest import Client
+
+
+API_KEY = "6228f7541bf40756eb2b5cfaa4e6f45f"
+API = "https://api.openweathermap.org/data/2.5/weather?q=Netanya, IL&appid=6228f7541bf40756eb2b5cfaa4e6f45f"
+response = requests.get(API)
+
+
+weather_repo = int((json.loads(response.content)["main"]["temp_max"]) - 273.13)
+
+
+# client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
+client = Client()
+
+# this is the Twilio sandbox testing number
+from_whatsapp_number = "whatsapp:+14155238886"
+# from_whatsapp_number = "whatsapp:+972535303588"
+# replace this number with your own WhatsApp Messaging number
+numbers = ["whatsapp:+972535303588", "whatsapp:+972547245344"]
+
+
+if weather_repo < 18:
+    weather_body = "*weather* for today in Netanya is : {0} Degrees  Wear something hot".format(
+        weather_repo
+    )
+
+else:
+    weather_body = "*The* Weather For today In Netanya is : {0} Degrees Wear Gufiya".format(
+        weather_repo
+    )
+for number in numbers:
+    client.messages.create(body=weather_body, from_=from_whatsapp_number, to=number)
