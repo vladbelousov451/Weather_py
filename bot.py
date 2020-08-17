c71adb47ebde78e54a0c0a17937a99f397ccdcde
vladibelousov54 <vladibelousov54@gmail.com>
2020-08-17 11:19:53 +0300

    add Bot functions

---
+++ bot.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+import json
+
+# CONDITIONS
+API = "https://api.openweathermap.org/data/2.5/weather?q=Netanya,IL&appid=6228f7541bf40756eb2b5cfaa4e6f45f"
+
+
+app = Flask(__name__)
+
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    incoming_msg = request.values.get("Body", "").lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if "weather" in incoming_msg:
+        temp = Get_weather()
+        msg.body(temp)
+        responded = True
+    if "quote" in incoming_msg:
+        # return a quote
+        r = requests.get("https://api.quotable.io/random")
+        if r.status_code == 200:
+            data = r.json()
+            quote = "{0} {1}".format(data["content"], data["author"])
+        else:
+            quote = "I could not retrieve a quote at this time, sorry."
+        msg.body(quote)
+        responded = True
+    if "cat" in incoming_msg:
+        # return a cat pic
+        msg.media("https://cataas.com/cat")
+        responded = True
+    if not responded:
+        msg.body("Text Me Something else Please")
+    return str(resp)
+
+
+def Get_weather():
+    weather = requests.get(API)
+    weather1 = json.loads(weather.content)
+    tempature = int((weather1["main"]["temp_max"]) - 273.13)
+    wind_speed = float(weather1["wind"]["speed"])
+    client = Client()
+    from_whatsapp_number = "whatsapp:+14155238886"
+    numbers = ["whatsapp:+972535303588", "whatsapp:+972547245344"]
+    weater_quote = "The tempature is {0} and the wind speed is {1} Km/h".format(
+        tempature, wind_speed
+    )
+    return weater_quote
+
+
+if __name__ == "__main__":
+    app.run()
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from twilio.rest import Client
 
 
 API_KEY = "6228f7541bf40756eb2b5cfaa4e6f45f"
-API = "https://api.openweathermap.org/data/2.5/weather?q=Netanya, IL&appid=6228f7541bf40756eb2b5cfaa4e6f45f"
+API = "https://api.openweathermap.org/data/2.5/weather?q=Netanya,IL&appid=6228f7541bf40756eb2b5cfaa4e6f45f"
 response = requests.get(API)
 
 
 weather_repo = int((json.loads(response.content)["main"]["temp_max"]) - 273.13)
-
+wind_speed = float(json.loads(response.content)["wind"]["speed"])
 
 # client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
 client = Client()
@@ -22,13 +22,13 @@ numbers = ["whatsapp:+972535303588", "whatsapp:+972547245344"]
 
 
 if weather_repo < 18:
-    weather_body = "*weather* for today in Netanya is : {0} Degrees  Wear something hot".format(
-        weather_repo
+    weather_body = "*weather* for today in Netanya is : {0} Degrees  Wear something hot and The wind Speed is {1} Km\h".format(
+        weather_repo, wind_speed
     )
 
 else:
-    weather_body = "*The* Weather For today In Netanya is : {0} Degrees Wear Gufiya".format(
-        weather_repo
+    weather_body = "*The* Weather For today In Netanya is : {0} Degrees Wear Gufiya and The wind Speed is {1} Km\h".format(
+        weather_repo, wind_speed
     )
 for number in numbers:
     client.messages.create(body=weather_body, from_=from_whatsapp_number, to=number)
