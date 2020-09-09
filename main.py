bda71178b68db750ef231857a29bd941e2ab2df8
vladbelousov451 <45417096+vladbelousov451@users.noreply.github.com>
2020-09-09 12:27:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
-import requests
-import json
-from twilio.rest import Client
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+from flask import Flask
+import time
+
+app = Flask(__name__)
 
 
-API_KEY = "6228f7541bf40756eb2b5cfaa4e6f45f"
-API = "https://api.openweathermap.org/data/2.5/weather?q=Netanya,IL&appid=6228f7541bf40756eb2b5cfaa4e6f45f"
-response = requests.get(API)
+@app.route('/')
+def index():
+    return 'Hellow world'
 
 
-weather_repo = int((json.loads(response.content)["main"]["temp_max"]) - 273.13)
-wind_speed = float(json.loads(response.content)["wind"]["speed"])
-
-# client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
-client = Client()
-
-# this is the Twilio sandbox testing number
-from_whatsapp_number = "whatsapp:+14155238886"
-# from_whatsapp_number = "whatsapp:+972535303588"
-# replace this number with your own WhatsApp Messaging number
-numbers = ["whatsapp:+972535303588", "whatsapp:+972547245344"]
+@app.route('/intance')
+def settimer():
+    getcpuover75()
 
 
-if weather_repo < 18:
-    weather_body = "*weather* for today in Netanya is : {0} Degrees  Wear something hot and The wind Speed is {1} Km\h".format(
-        weather_repo, wind_speed
-    )
+def getcpuover75():
+    processces = cpu_count()
+    pool = Pool(processces)
+    pool.map(overcpu(10), range(processces))
+    app.route('/')
 
-else:
-    weather_body = "*The* Weather For today In Netanya is : {0} Degrees Wear Gufiya and The wind Speed is {1} Km\h".format(
-        weather_repo, wind_speed
-    )
-for number in numbers:
-    client.messages.create(body=weather_body, from_=from_whatsapp_number, to=number)
+
+def overcpu(x):
+    endtime = time.time() + 60
+    while time.time() < endtime:
+        x*x
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
